8fa513a8329c927b0b92baa40ffe1a36d0331e77
Nela-cat <74066037+Nela-cat@users.noreply.github.com>
2022-11-23 16:58:02 +0100

    Update lab.py

--- prog_lab/lab.py
+++ prog_lab/lab.py
@@ -108,3 +108,31 @@ if ans:
     print("Yes")
 else:
     print("No")
+    
+    
+    
+    
+    
+    
+oc = int(input("Pod. il. ocen\n"))
+lista1 =[]
+i=0
+
+while i<oc:
+    m=int(input("Podaj oceny\n"))
+    lista1.append(m)
+    i+=1
+print(lista1)
+
+def fun():
+    suma=sum(lista1)
+    print(f"suma: {suma}")
+    pod=int(input("pod. el.\n"))
+    lista1.append(pod)
+    print(lista1)
+    f = lista1.pop(-1)
+    
+    print(lista1)
+    print(f"{f} wyd. el")
+fun()        
+    
